378bd0180a52b9fe3ba17147826c7bad0a51bfb8
unknown <crivellis@udesa.edu.ar>
2026-04-22 19:24:36 -0300

    fase de frecuencia de términos + gráfico

---
+++ TP2/scripts/metrics_figures.R
@@ -0,0 +1,63 @@
+oea_tfidf <- lema_oea_df |>
+  count(doc_id, lemma) |>
+  bind_tf_idf(doc_id, lemma, n)
+
+n_docs <- 6
+
+oea_destacados <- oea_tfidf |>
+  group_by(lemma) |>
+  summarise(
+    tfidf_promedio = mean(tf_idf),
+    nro_documentos = n_distinct(doc_id),
+    promedio_tf = mean(tf),
+    promedio_idf = mean(idf),
+    frecuencia = sum(n)
+  ) |>
+  filter(nro_documentos >= n_docs)|>
+  arrange(desc(tfidf_promedio)) 
+
+print(oea_destacados, n = 80)
+
+terminos <- c("democracia", "educación", "ciberseguridad", "indígena", "elección")
+
+n_terminos <- lema_oea_df |>
+  filter(lemma %in% terminos) |>
+  group_by(lemma) |>
+  summarise(
+    n = n(),
+    nro_documentos = n_distinct(id)
+  ) |>
+  arrange(desc(n))
+
+
+n_terminos |>
+  select(lemma, n) |>
+  head()
+
+freq_terminos <- ggplot(
+  n_terminos, 
+  aes(x = reorder (lemma, n), y = n)) +
+  geom_col(fill = "darkred" , alpha = 0.8) +
+  labs(
+    title = "Comunicados de la OEA: términos elegidos",
+    subtitle = "Comparación de los términos seleccionados",
+    x = "Términos seleccionados",
+    y = "Frecuencia total",
+    caption = "Fuente: Organización de los Estados Americanos (OEA)"
+  ) +
+  theme_minimal(base_size = 12) +
+  coord_flip() +
+  scale_y_continuous(breaks = seq(0, max(80), by = 10)) +
+  theme(
+    plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+    plot.subtitle = element_text(size = 12, color = "black", hjust = 0.5),
+    axis.text.x = element_text(size = 12),
+    axis.text.y = element_text(size = 12),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+
+ggsave(
+   file = file.path(output_dir, "frecuencia_terminos.png"),
+   plot = freq_terminos
+ )
